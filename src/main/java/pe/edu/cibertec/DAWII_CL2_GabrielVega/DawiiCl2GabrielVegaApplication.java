715173851bdfb4993f0c58f74d7b2592f04dfb69
Gabomou07 <gabriel.vegabalcazar@gmail.com>
2023-10-28 16:50:09 -0500

    Estructura del Proyecto

---
+++ src/main/java/pe/edu/cibertec/DAWII_CL2_GabrielVega/DawiiCl2GabrielVegaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_CL2_GabrielVega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl2GabrielVegaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl2GabrielVegaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DAWII_CL2_GabrielVega/DawiiCl2GabrielVegaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_CL2_GabrielVega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiCl2GabrielVegaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
